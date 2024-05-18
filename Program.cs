9537e3990de6cbf3103b6bca6cb319e1249a026d
Xavier <pedrooohhenriquex@hotmail.com>
2024-05-17 21:42:53 -0300

    progress

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("--- Entrada Válida ---");
+
+String opcao = "";
+while (opcao <= 9)
+{
+    Console.Write("Digite um número entre 1 e 9 (ou 0 para cancelar)...:");
+    Console.ReadLine
+ while 
+}
